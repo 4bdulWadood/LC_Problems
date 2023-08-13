86fcda6ba87febb0d81a8585f0376f576b58f3ac
Syed Wadood <57127141+4bdulWadood@users.noreply.github.com>
2023-08-13 16:20:07 -0400

    Create powerset.js

---
+++ powerset.js
@@ -0,0 +1,30 @@
+/*
+    Write a function to return all subsets of a set
+    [1,2,3]
+
+    Time Complexity : This is a balanced and perfect binary tree so the number of nodes will be 2^N, where N is the height of the tree and the length of the input array.
+                        the time complexity to visit 2^N nodes will be O(2^N) , where N is the length of the input. Addtionally the data in the subset is being combined with 
+                        set[depth] element for each recursive iteration. So in the worst case the time complexity is O(2^N*N)
+
+    Space Complexity : Since we are performing recursion the memory stack will hold data from previous recursive calls.
+                       In the worst case the memory stack will hold data from N values, where N is the length of the input. Each recursive iteration holds N values at worst case. The
+                       time complexity is O(2*2^n)
+
+    Memoization cannot be used to make the algorithm more efficient as each subset being generated (each path in the binary tree to the bottom) is unique.
+
+    
+*/
+
+var set = [1,2,3];
+var powerSet = [[]];
+
+function getSubsets(set, subset=[], depth=0){
+        if(set.length===depth){
+            powerSet.push(subset);
+            return
+        }
+        getSubsets(set, subset, depth+1);
+        getSubsets(set, [...subset, set[depth]], depth+1);
+}
+getSubsets(set)
+console.log(powerSet)
