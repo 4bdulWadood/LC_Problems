e9730f3fd63598ae89bc40109891034767d45c4f
Syed Wadood <57127141+4bdulWadood@users.noreply.github.com>
2023-09-21 12:22:31 -0400

    Update NextLexicographicalString.js

--- NextLexicographicalString.js
+++ NextLexicographicalString.js
@@ -13,6 +13,25 @@
     only difference is characters used here. Maybe characters can be compared using ASCII values?
 */
 
+/*
+    Name: find next permutation of string
+    Type: Lexicographical Permutation
+
+    You are given a string of lowercase letters. Your task is to find the lexicographically next
+    greater permutation of the given string. If no such permutation exists, return the string
+    in its lexicographically smallest form.
+
+    Input: abdc
+    Output: acbd
+
+    Similar to finding the next lexigraphical permutation in a numerical array problem,
+    only difference is characters used here. Maybe characters can be compared using ASCII values?
+
+    The time complexity of this algorithm is O(N)
+    The space complexity of this algorithm is O(N)
+
+    */
+
 
 var str = "helloguy";
 var save;
@@ -37,11 +56,9 @@ for(var p = save+1; p < str.length; p++){
     }
 }
 
-console.log(min, save)
-
 //Strings are immutable in JS so you cannot manually swap characters by assignment.
 //You can convert strings to array then do the swapping and then convert the array back to a string.
-
+/*
 var arr = str.split("");
 
 var s = arr[save];
@@ -49,6 +66,14 @@ arr[save] = arr[min];
 arr[min] = s;
 
 str = arr.join("");
+
+Instead of Converting the string to a mutable data type and performing the swapping, we can use the .slice method to 
+recreate a new string without increasing the space complexity by creating a new array object.
+*/
+
+var temp = str[save];
+
+str = str.slice(0, save) + str[min] + str.slice(save+1, min) + str[save] +  str.slice(min+1, str.length);
 
 var preStr = str.split("").slice(0, save+1).join("");
 var postStr = str.split("").slice(save+1, p+1).reverse().join("");
